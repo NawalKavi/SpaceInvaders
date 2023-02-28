929467dcfbb6577e8ab58697d6f366668595f4e4
Nawal <nawalkavi@gmail.com>
2023-02-28 16:13:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,18 @@ while gameRunning == True:
         # Quits the game by closing the window.
         if gameEvent.type == QUIT:
             gameRunning = False
-
+1
 class Object():
-    def __init__(self, )
+    def __init__(self, sizeX, sizeY):
+        self.Size = [sizeX, sizeY]
+        self.CentrePosition = [sizeX / 2, sizeY / 2]
+        self.AbsolutePosition = [0,0]
+
+    def editPosition(self, newX, newY):
+        self.AbsolutePosition[0] = newX - self.Size[0] / 2
+        self.AbsolutePosition[1] = newY - self.Size[1] / 2
+        self.CentrePosition[0] = newX
+        self.CentrePosition[1] = newY
 
     # Fills the window with the specified colour (black).
     mainWindow.fill((0, 0, 0))
