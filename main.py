6b961ce6cba8826fddf9b2e55d01401f10e18be1
Nawal <nawalkavi@gmail.com>
2023-03-03 12:13:02 +0000

    kill me

--- main.py
+++ main.py
@@ -15,22 +15,40 @@ imageDictionary = {
 "player": transform.scale(image.load("Images//Player.png"), (40, 40))
 }
 
+invaderArray = []
+
 class Object():
-	def __init__(self, sizeX, sizeY):
+	def __init__(self, objectName, sizeX, sizeY):
 		self.Size = [sizeX, sizeY]
 		self.CentrePosition = [sizeX / 2, sizeY / 2]
 		self.AbsolutePosition = [0,0]
-
+		self.ObjectName = objectName
+		
 	def editPosition(self, newX, newY):
 		self.AbsolutePosition[0] = newX - self.Size[0] / 2
 		self.AbsolutePosition[1] = newY - self.Size[1] / 2
 		self.CentrePosition[0] = newX
 		self.CentrePosition[1] = newY
-
+		
+class Invader(Object):
+	def __init__(self, objectName, sizeX, sizeY, invaderType):
+		self.invaderType = invaderType
+		super().__init__(objectName, 5, 20)
+		invaderArray.append(self)
+				
+				
+def generateInvader():
+	for row in range(0, 6):
+		for column in range(0, 17):
+			newInvader = Invader("Invader", 40, 40, "invader1")
+			newInvader.editPosition((column+1)*40, 75+(row+1)*40)
 
 # Game loop.
 oneSecondTick = 0
 gameRunning = True
+
+generateInvader()
+print(invaderArray)
 while gameRunning == True:
 	for gameEvent in event.get():
 		# Quits the game by closing the window.
@@ -43,6 +61,9 @@ while gameRunning == True:
 
 	# Fills the window with the specified colour (black).
 	mainWindow.fill((0, 0, 0))
+	
+	for invaderValue in invaderArray:
+		mainWindow.blit(imageDictionary[invaderValue.invaderType], (invaderValue.AbsolutePosition[0], invaderValue.AbsolutePosition[1]))
 
 	# Runs game at 120fps.
 	time.Clock().tick(120)
