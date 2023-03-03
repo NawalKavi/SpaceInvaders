720ebeee603c2402e6bbedfa0c06aadeffe73a1b
Nawal <nawalkavi@gmail.com>
2023-03-03 12:01:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -15,32 +15,37 @@ imageDictionary = {
 "player": transform.scale(image.load("Images//Player.png"), (40, 40))
 }
 
+class Object():
+	def __init__(self, sizeX, sizeY):
+		self.Size = [sizeX, sizeY]
+		self.CentrePosition = [sizeX / 2, sizeY / 2]
+		self.AbsolutePosition = [0,0]
+
+	def editPosition(self, newX, newY):
+		self.AbsolutePosition[0] = newX - self.Size[0] / 2
+		self.AbsolutePosition[1] = newY - self.Size[1] / 2
+		self.CentrePosition[0] = newX
+		self.CentrePosition[1] = newY
+
+
 # Game loop.
 oneSecondTick = 0
 gameRunning = True
 while gameRunning == True:
-    for gameEvent in event.get():
-        # Quits the game by closing the window.
-        if gameEvent.type == QUIT:
-            gameRunning = False
-1
-class Object():
-    def __init__(self, sizeX, sizeY):
-        self.Size = [sizeX, sizeY]
-        self.CentrePosition = [sizeX / 2, sizeY / 2]
-        self.AbsolutePosition = [0,0]
+	for gameEvent in event.get():
+		# Quits the game by closing the window.
+		if gameEvent.type == QUIT:
+			gameRunning = False
+		# Handles keyboard actions.
+		if gameEvent.type == KEYDOWN:
+			if gameEvent.key == K_UP:
+				pass
 
-    def editPosition(self, newX, newY):
-        self.AbsolutePosition[0] = newX - self.Size[0] / 2
-        self.AbsolutePosition[1] = newY - self.Size[1] / 2
-        self.CentrePosition[0] = newX
-        self.CentrePosition[1] = newY
+	# Fills the window with the specified colour (black).
+	mainWindow.fill((0, 0, 0))
 
-    # Fills the window with the specified colour (black).
-    mainWindow.fill((0, 0, 0))
+	# Runs game at 120fps.
+	time.Clock().tick(120)
 
-    # Runs game at 120fps.
-    time.Clock().tick(120)
-
-    # Displays the output.
-    display.flip()
+	# Displays the output.
+	display.flip()
